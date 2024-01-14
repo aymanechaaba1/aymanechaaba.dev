253016eef82747a6ade5583e0876caf3e8332e0e
Aymane Chaaba <aymanechaaba1@gmail.com>
2024-01-14 13:02:57 +0100

    highlight navlink

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import Link from 'next/link';
 import { ToggleDarkMode } from './ToggleDarkMode';
+import { usePathname, useSearchParams } from 'next/navigation';
+import { cn } from '@/lib/utils';
 
 const navlinks = [
   {
@@ -7,7 +11,7 @@ const navlinks = [
     label: 'home',
   },
   {
-    path: '/projects/?sort=updated&per_page=5&page=1',
+    path: '/projects?sort=updated&per_page=5&page=1',
     label: 'projects',
   },
   {
@@ -21,6 +25,8 @@ const navlinks = [
 ];
 
 function Navbar() {
+  const pathname = usePathname();
+
   return (
     <nav className="flex items-center justify-end gap-5">
       <div className="flex items-center gap-4">
@@ -28,7 +34,12 @@ function Navbar() {
           <Link
             key={navlink.path}
             href={navlink.path}
-            className="scroll-m-20 tracking-tight font-semibold"
+            className={cn(
+              'scroll-m-20 tracking-tight font-semibold text-gray-500',
+              {
+                'text-white': navlink.path.split('?').at(0) === pathname,
+              }
+            )}
           >
             {navlink.label}
           </Link>
