29b362e9acbfe92ecbbf6e584597082ef98c56f4
Aymane Chaaba <aymanechaaba1@gmail.com>
2024-05-08 13:13:21 +0100

    header

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,42 +1,12 @@
 'use client';
 
 import Link from 'next/link';
-import { ToggleDarkMode } from './ToggleDarkMode';
-import { usePathname, useSearchParams } from 'next/navigation';
-import { cn } from '@/lib/utils';
-
-const navlinks = [
-  {
-    path: '/projects',
-    label: 'Projects',
-  },
-];
+import NavLinks from './NavLinks';
 
 function Navbar() {
-  const pathname = usePathname();
-
   return (
-    <nav className="flex items-center justify-end gap-5">
-      <div className="flex items-center gap-4">
-        {navlinks.map((navlink) => (
-          <Link
-            key={navlink.path}
-            href={navlink.path}
-            className={cn(
-              'scroll-m-20 tracking-tight font-semibold text-gray-500',
-              {
-                'dark:text-white text-gray-900':
-                  navlink.path.split('?').at(0) === pathname,
-              }
-            )}
-          >
-            {navlink.label}
-          </Link>
-        ))}
-      </div>
-      <div>
-        <ToggleDarkMode />
-      </div>
+    <nav className="flex flex-col gap-y-3 mt-3">
+      <NavLinks />
     </nav>
   );
 }
