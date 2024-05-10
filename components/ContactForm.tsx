d6e669f8f78c0e3b52fb171978630c38b098671e
Aymane Chaaba <aymanechaaba1@gmail.com>
2024-05-10 13:14:18 +0100

    contact

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { Label } from './ui/label';
-import { Input } from './ui/input';
 import {
   Select,
   SelectContent,
@@ -13,11 +12,12 @@ import {
 } from './ui/select';
 import { cn, formatBudget, formatLastBudget } from '@/lib/utils';
 import { Textarea } from './ui/textarea';
-import { Button } from './ui/button';
 import { useFormState, useFormStatus } from 'react-dom';
 import { useEffect, useRef } from 'react';
 import { toast } from 'sonner';
 import { submitForm } from '@/actions/submitForm';
+import { Button as HeadlessBtn, Field, Input } from '@headlessui/react';
+import { Button } from './ui/button';
 
 export const projectsStartDates = {
   asap: 'as soon as possible',
@@ -38,7 +38,7 @@ function SendButton() {
   const { pending } = useFormStatus();
 
   return (
-    <Button type="submit" disabled={pending}>
+    <Button type="submit" className="md:col-span-2" disabled={pending}>
       {pending ? 'Sending...' : 'Send'}
     </Button>
   );
@@ -57,151 +57,158 @@ function ContactForm() {
   }, [state]);
 
   return (
-    <form
-      action={formAction}
-      className="my-5 grid grid-cols-1 md:grid-cols-2 gap-4"
-      ref={formRef}
-    >
-      <div className="space-y-2">
-        <Label htmlFor="firstname" className="font-medium">
-          First Name
-        </Label>
-        <Input
-          id="firstname"
-          name="firstname"
-          className={cn({
-            'border-red-500': state?.errors?.firstname,
-          })}
-        />
-        {state?.errors?.firstname && (
-          <span className="text-xs text-red-500">{state.errors.firstname}</span>
-        )}
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="lastname" className="font-medium">
-          Last Name
-        </Label>
-        <Input
-          id="lastname"
-          name="lastname"
-          className={cn({
-            'border-red-500': state?.errors?.lastname,
-          })}
-        />
-        {state?.errors?.lastname && (
-          <span className="text-xs text-red-500">{state.errors.lastname}</span>
-        )}
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="company_name" className="font-medium">
-          Company Name
-        </Label>
-        <Input
-          id="company_name"
-          name="company_name"
-          className={cn({
-            'border-red-500': state?.errors?.company_name,
-          })}
-        />
-        {state?.errors?.company_name && (
-          <span className="text-xs text-red-500">
-            {state.errors.company_name}
-          </span>
-        )}
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="email" className="font-medium">
-          Email
-        </Label>
-        <Input
-          id="email"
-          name="email"
-          className={cn({
-            'border-red-500': state?.errors?.email,
-          })}
-        />
-        {state?.errors?.email && (
-          <span className="text-xs text-red-500">{state.errors.email}</span>
-        )}
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="project_start_date" className="font-medium">
-          Estimated Project Start Date
-        </Label>
-        <Select name="project_start_date" defaultValue="asap">
-          <SelectTrigger
-            className={cn('w-full', {
-              'border-red-500': state?.errors?.project_start_date,
+    <section className="flex flex-col md:flex-row md:items-center md:justify-around">
+      <h1 className="ac-heading text-center">Let&apos;s Work Together</h1>
+      <form
+        action={formAction}
+        className="my-5 grid grid-cols-1 md:grid-cols-2 gap-4"
+        ref={formRef}
+      >
+        <Field>
+          <Label htmlFor="firstname" className="font-medium">
+            First Name
+          </Label>
+          <Input
+            id="firstname"
+            name="firstname"
+            className={cn('headless-input', {
+              'border-red-500': state?.errors?.firstname,
             })}
-          >
-            <SelectValue placeholder="Select an estimated date" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              {Object.entries(projectsStartDates).map(([key, value], i) => (
-                <SelectItem key={i} value={key}>
-                  {value}
-                </SelectItem>
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        {state?.errors?.project_start_date && (
-          <span className="text-xs text-red-500">
-            {state.errors.project_start_date}
-          </span>
-        )}
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="budget" className="font-medium">
-          Budget
-        </Label>
-        <Select name="budget" defaultValue={budgets[0]}>
-          <SelectTrigger
-            className={cn('w-full', {
-              'border-red-500': state?.errors?.budget,
+          />
+          {state?.errors?.firstname && (
+            <span className="text-xs text-red-500">
+              {state.errors.firstname}
+            </span>
+          )}
+        </Field>
+        <Field>
+          <Label htmlFor="lastname" className="font-medium">
+            Last Name
+          </Label>
+          <Input
+            id="lastname"
+            name="lastname"
+            className={cn('headless-input', {
+              'border-red-500': state?.errors?.lastname,
             })}
-          >
-            <SelectValue placeholder="Select an estimated date" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              {budgets.slice(0, -1).map((budget, i) => (
-                <SelectItem key={i} value={budget}>
-                  {formatBudget(budget)}
+          />
+          {state?.errors?.lastname && (
+            <span className="text-xs text-red-500">
+              {state.errors.lastname}
+            </span>
+          )}
+        </Field>
+        <Field>
+          <Label htmlFor="company_name" className="font-medium">
+            Company Name
+          </Label>
+          <Input
+            id="company_name"
+            name="company_name"
+            className={cn('headless-input', {
+              'border-red-500': state?.errors?.company_name,
+            })}
+          />
+          {state?.errors?.company_name && (
+            <span className="text-xs text-red-500">
+              {state.errors.company_name}
+            </span>
+          )}
+        </Field>
+        <Field>
+          <Label htmlFor="email" className="font-medium">
+            Email
+          </Label>
+          <Input
+            id="email"
+            name="email"
+            className={cn('headless-input', {
+              'border-red-500': state?.errors?.email,
+            })}
+          />
+          {state?.errors?.email && (
+            <span className="text-xs text-red-500">{state.errors.email}</span>
+          )}
+        </Field>
+        <Field>
+          <Label htmlFor="project_start_date" className="font-medium">
+            Estimated Project Start Date
+          </Label>
+          <Select name="project_start_date" defaultValue="asap">
+            <SelectTrigger
+              className={cn('w-full mt-[.75rem]', {
+                'border-red-500': state?.errors?.project_start_date,
+              })}
+            >
+              <SelectValue placeholder="Select an estimated date" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                {Object.entries(projectsStartDates).map(([key, value], i) => (
+                  <SelectItem key={i} value={key}>
+                    {value}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          {state?.errors?.project_start_date && (
+            <span className="text-xs text-red-500">
+              {state.errors.project_start_date}
+            </span>
+          )}
+        </Field>
+        <Field>
+          <Label htmlFor="budget" className="font-medium">
+            Budget
+          </Label>
+          <Select name="budget" defaultValue={budgets[0]}>
+            <SelectTrigger
+              className={cn('w-full mt-[.75rem]', {
+                'border-red-500': state?.errors?.budget,
+              })}
+            >
+              <SelectValue placeholder="Select an estimated date" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                {budgets.slice(0, -1).map((budget, i) => (
+                  <SelectItem key={i} value={budget}>
+                    {formatBudget(budget)}
+                  </SelectItem>
+                ))}
+                <SelectItem value={budgets.at(-1)!}>
+                  {formatLastBudget(budgets.at(-1)!)}
                 </SelectItem>
-              ))}
-              <SelectItem value={budgets.at(-1)!}>
-                {formatLastBudget(budgets.at(-1)!)}
-              </SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        {state?.errors?.budget && (
-          <span className="text-xs text-red-500">{state.errors.budget}</span>
-        )}
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="brief_description" className="font-medium">
-          Brief description of your project scope
-        </Label>
-        <Textarea
-          id="brief_description"
-          name="brief_description"
-          placeholder="Maximum of 2000 characters."
-          className={cn({
-            'border-red-500': state?.errors?.brief_description,
-          })}
-        />
-        {state?.errors?.brief_description && (
-          <span className="text-xs text-red-500">
-            {state.errors.brief_description}
-          </span>
-        )}
-      </div>
-      <div />
-      <SendButton />
-    </form>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          {state?.errors?.budget && (
+            <span className="text-xs text-red-500">{state.errors.budget}</span>
+          )}
+        </Field>
+        <Field className="md:col-span-2">
+          <Label htmlFor="brief_description" className="font-medium">
+            Brief description of your project scope
+          </Label>
+          <Textarea
+            id="brief_description"
+            name="brief_description"
+            placeholder="Maximum of 2000 characters."
+            className={cn('mt-[.75rem]', {
+              'border-red-500': state?.errors?.brief_description,
+            })}
+          />
+          {state?.errors?.brief_description && (
+            <span className="text-xs text-red-500">
+              {state.errors.brief_description}
+            </span>
+          )}
+        </Field>
+        <div />
+        <SendButton />
+      </form>
+    </section>
   );
 }
 
