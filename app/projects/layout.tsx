f1c71f42a26fb67f9c7a599eb8ef005f86e45ebc
Aymane Chaaba <aymanechaaba1@gmail.com>
2024-05-15 13:48:57 +0100

    articles

--- app/projects/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactNode } from 'react';
-
-function ProjectsLayout({ children }: { children: Readonly<ReactNode> }) {
-  return <div className="">{children}</div>;
-}
-
-export default ProjectsLayout;
