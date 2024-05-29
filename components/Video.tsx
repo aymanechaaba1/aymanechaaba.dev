4e0c3d7df944833085c2298be9088c4608cb8dce
Aymane Chaaba <aymanechaaba1@gmail.com>
2024-05-29 13:43:36 +0100

    vid

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,15 +1,31 @@
-import { cn } from '@/lib/utils';
-import { PlayerProps } from 'next-video';
-import { RefObject, forwardRef } from 'react';
+'use client';
 
-function Video(props: { src: string; className: string }) {
+import { cn } from '@/lib/utils';
+import { useRef, useState } from 'react';
+
+function Video(props: { src: string; className?: string }) {
+  const [isMuted, setIsMuted] = useState(true);
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const handleToggleMuted = () => {
+    if (videoRef.current) {
+      videoRef.current.muted = !isMuted;
+
+      setIsMuted(!isMuted);
+    }
+  };
+
   return (
     <video
-      className={cn(props.className)}
-      src={props.src}
-      controls
-      muted
+      onClick={handleToggleMuted}
+      ref={videoRef}
+      className={cn('mx-auto rounded-2xl mt-5', props.className)}
+      width={350}
+      height={500}
+      muted={isMuted}
       autoPlay
+      playsInline
       loop
     >
       <source src={props.src} type="video/mp4" />
